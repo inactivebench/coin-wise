1de6708de327b4b94916890fac9d627797278993
inactivebench <emmanuelmwangi86@gmail.com>
2024-09-26 16:02:07 +0300

    fixed alingment in card & hero section

--- client/src/components/FlashcardList.jsx
+++ client/src/components/FlashcardList.jsx
@@ -10,11 +10,9 @@ const FlashcardList = () => {
   return (
     <section className='card-section'>
       <div className='container grid card-container  '>
-        <article className='card-section-title grid'>
-          <h1 className=' ff-montserrat fs-600'>
-            Four rules for less money stress{" "}
-          </h1>
-          <p>
+        <article className='card-section-title grid ff-montserrat'>
+          <h1 className='  fs-600'>Four rules for less money stress </h1>
+          <p className='fs-300'>
             Acquire self-assurance and clarity by sticking to four basic yet
             transformative guidelines. The magic is in the approach.
           </p>
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <section className='section hero '>
-      <div className='container flex'>
-        <article className='hero-info '>
+      <div className='container hero-container flex'>
+        <article className='hero-info grid '>
           <h1 className='hero-title'>
             Money Made<br></br> <span className='text-accent'>Easy</span>
           </h1>
@@ -16,8 +16,8 @@ const Hero = () => {
             <button className=' btn capitalize'>start your free trial</button>
           </Link>
         </article>
-        <article className='hero--img'>
-          <img src={heroImg} alt='payment illustration' className='hero-img' />
+        <article className='hero-img'>
+          <img src={heroImg} alt='payment illustration' />
         </article>
       </div>
     </section>
