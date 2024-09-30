5ce09776faef70708a9a0098eda3f7b9eed8fa34
inactivebench <emmanuelmwangi86@gmail.com>
2024-09-30 18:48:39 +0300

    added log in page

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,4 +1,78 @@
+import { useRef, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 const Signin = () => {
-  return <div>Sign in here</div>;
+  const emailRef = useRef();
+  const errRef = useRef();
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [email, pwd]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className='signup-section-container'>
+      <Link to='/' className='logo  sign-logo'>
+        coin-wise
+      </Link>
+      <div className='form-container flex'>
+        <div>
+          <h1 className='form-header capitalize'>log in</h1>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live='assertive'
+          >
+            {errMsg}
+          </p>
+        </div>
+        <form className='flex form' onSubmit={handleSubmit}>
+          <label htmlFor='email' className=' label'>
+            Email Address
+          </label>
+          <input
+            type='email'
+            id='email'
+            ref={emailRef}
+            autoComplete='off'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className='input'
+            required
+          />
+
+          <label htmlFor='password' className='label'>
+            Password
+          </label>
+          <input
+            type='password'
+            id='password'
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            className='input'
+            required
+          />
+
+          <button className='btn capitalize'>log in</button>
+        </form>
+        <p className='link-p '>
+          New to coin-wise?
+          <span className='link-span'>
+            <Link to='/signup'>Sign Up today</Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 export default Signin;
