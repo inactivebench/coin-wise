fd421052a79a7918fac0165b032241a4c5f46e21
inactivebench <emmanuelmwangi86@gmail.com>
2024-10-29 12:00:23 +0300

    Refactor: moved profile and signout links to header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,83 @@
-import { FaRegUser } from "react-icons/fa";
-import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
 import useAuth from "../hook/useAuth";
+import useLogout from "../hook/useLogout";
+import { jwtDecode } from "jwt-decode";
+import { Link, useNavigate } from "react-router-dom";
+import { TbLogout2 } from "react-icons/tb";
+import * as FaIcons from "react-icons/fa";
+import Modal from "./Modal";
 
-const Header = ({ pageTitle }) => {
+const Header = ({ pageTitle, location }) => {
+  const [dropdown, setDropdown] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const navigate = useNavigate();
+  const logout = useLogout();
   const { auth } = useAuth();
+
   const decoded = auth?.accessToken ? jwtDecode(auth.accessToken) : undefined;
   const user = decoded.userInfo.user;
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <div className='header-main flex  fs-300 '>
       <h1 className='capitalize '>{pageTitle}</h1>
-      <div className='flex header-info'>
-        <FaRegUser />
+      <div
+        className='flex header-info'
+        onClick={() => {
+          setDropdown(!dropdown);
+        }}
+      >
+        <FaIcons.FaRegUser />
         <h2>{user}</h2>
       </div>
+      {dropdown && (
+        <div className='dropdown-menu'>
+          <ul className='flex'>
+            <li
+              className={`flex capitalize text-white dropdown-link  ${
+                location.pathname === "/profile" && "active"
+              }`}
+            >
+              <Link to='/profile' className='flex link'>
+                <FaIcons.FaRegUser size={26} />
+                <span className='fs-400'>profile</span>
+              </Link>
+            </li>
+            <li
+              className={`flex capitalize text-white logout dropdown-link `}
+              onClick={openModal}
+            >
+              <TbLogout2 size={26} />
+              <span className='fs-400'>sign out</span>
+            </li>
+          </ul>
+        </div>
+      )}
+      <Modal isOpen={modalOpen} onClose={closeModal} modalTitle={"Sign out ?"}>
+        <div className='flex modal-body'>
+          <h2>Do you want to want to sign out ?</h2>
+        </div>
+        <div className='flex modal-btn'>
+          <button className='capitalize cancel' onClick={closeModal}>
+            cancel
+          </button>
+          <button className='capitalize signout' onClick={handleLogout}>
+            sign out
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-import { useNavigate, Link, useLocation } from "react-router-dom";
-import useLogout from "../hook/useLogout";
+import { useLocation } from "react-router-dom";
 import SidebarMenu from "./SidebarMenu";
 import control from "../assets/icons/control.png";
 import logo from "../assets/images/coin.svg";
-import { TbLogout2 } from "react-icons/tb";
 import * as FaIcons from "react-icons/fa";
 import * as IoIcons from "react-icons/io";
-import Modal from "./Modal";
 import Header from "./Header";
 
 const sidebarMenuList = [
@@ -24,27 +21,12 @@ const sidebarMenuList = [
   },
 ];
 const Sidebar = ({ pageTitle }) => {
-  const navigate = useNavigate();
   const location = useLocation();
-  const logout = useLogout();
   const [isOpen, setIsOpen] = useState(true);
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const handleLogout = () => {
-    logout();
-    navigate("/");
-  };
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-  const closeModal = () => {
-    setModalOpen(false);
-  };
 
   return (
     <>
-      <Header pageTitle={pageTitle} />
+      <Header pageTitle={pageTitle} location={location} />
       <aside
         className={`sidebar bg-grey  ${isOpen ? "open" : "close "} fs-400`}
         aria-label='sidebar'
@@ -65,46 +47,6 @@ const Sidebar = ({ pageTitle }) => {
             return <SidebarMenu menu={menu} key={index} isOpen={isOpen} />;
           })}
         </ul>
-        <div>
-          <li
-            className={`flex capitalize text-white sidebar-menu ${
-              !isOpen && "hidden"
-            } ${location.pathname === "/profile" && "active"}`}
-            data-hover='profile'
-          >
-            <Link to='/profile' className='flex link'>
-              <FaIcons.FaRegUser size={28} />
-              <span className={`${!isOpen && "hidden"}`}>profile</span>
-            </Link>
-          </li>
-          <li
-            className={`flex capitalize text-white logout sidebar-menu ${
-              !isOpen && "hidden"
-            }`}
-            data-hover='sign out'
-            onClick={openModal}
-          >
-            <TbLogout2 size={28} />
-            <span className={` ${!isOpen && "hidden"}`}>sign out</span>
-          </li>
-        </div>
-        <Modal
-          isOpen={modalOpen}
-          onClose={closeModal}
-          modalTitle={"Sign out ?"}
-        >
-          <div className='flex modal-body'>
-            <h2>Do you want to want to sign out ?</h2>
-          </div>
-          <div className='flex modal-btn'>
-            <button className='capitalize cancel' onClick={closeModal}>
-              cancel
-            </button>
-            <button className='capitalize signout' onClick={handleLogout}>
-              sign out
-            </button>
-          </div>
-        </Modal>
       </aside>
     </>
   );
