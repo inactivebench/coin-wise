ac79ffea3d294eebcaf11fa79e648d8745dfffb1
inactivebench <emmanuelmwangi86@gmail.com>
2024-11-27 18:38:02 +0300

    Refactor: pie chart to display label at the center

--- client/src/components/budget-tracking/spending/PieChart.jsx
+++ client/src/components/budget-tracking/spending/PieChart.jsx
@@ -6,6 +6,7 @@ import {
   Pie,
   Cell,
   Tooltip,
+  Label,
   Legend,
   ResponsiveContainer,
 } from "recharts";
@@ -14,6 +15,17 @@ const CategoryPieChart = () => {
   const CATEGORY_URL = "/transaction/category";
   const [pieData, setPieData] = useState([]);
   const { auth } = useAuth();
+  const COLORS = [
+    "#003F5C",
+    "#58508d",
+    "#9B3192",
+    "#bc5090",
+    "#ff6361",
+    "#ffa600",
+    "#F7B7A3",
+    "#939b90",
+    "#cde492",
+  ];
 
   const fetchCategories = async () => {
     try {
@@ -38,9 +50,9 @@ const CategoryPieChart = () => {
   useEffect(() => {
     fetchCategories();
   }, []);
+
   return (
-    <div>
-      <h2>pie chart</h2>
+    <div className='flex'>
       <PieChart width={500} height={500}>
         <Pie
           data={pieData}
@@ -48,20 +60,61 @@ const CategoryPieChart = () => {
           nameKey='category'
           cx='50%'
           cy='50%'
-          innerRadius={60}
+          innerRadius={100}
           outerRadius={160}
           fill='#000000'
         >
           {pieData.map((entry, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={`#${Math.floor(Math.random() * 16777215).toString(16)}`}
-            />
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
+
+          <Label
+            className='uppercase'
+            value='total spending'
+            position='centerBottom'
+          />
+          <Label
+            value={pieData.reduce((acc, curr) => {
+              let total = acc + curr.total_amount;
+              return total;
+            }, 0)}
+            position='centerTop'
+          />
         </Pie>
         <Tooltip nameKey='category' />
         <Legend nameKey='category' />
       </PieChart>
+      <div>
+        spending breakdown
+        <div>Total Spending</div>
+        <table className='table-spend'>
+          <thead>
+            <tr>
+              <th className='table-header'>
+                <p className='uppercase'>category</p>
+              </th>
+              <th className='table-header'>
+                <p className='uppercase'>spending</p>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {pieData.map((item, index) => {
+              const { category, total_amount } = item;
+              return (
+                <tr key={index}>
+                  <td>
+                    <p>{category}</p>
+                  </td>
+                  <td>
+                    <p>{total_amount}</p>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
